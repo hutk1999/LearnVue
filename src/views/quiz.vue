7b55c84fe0673457362eb7d90a8eb2ff52702341
itamarshubin <50084547+itamarshubin@users.noreply.github.com>
2019-08-04 02:58:27 +0300

    add the quiz path

---
+++ src/views/quiz.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="quiz">
+      <Header />
+      <QustionBox />
+    <h1>quiz page</h1>
+  </div>
+</template>
+<script>
+import Header from '../components/Header.vue'
+import QustionBox from '../components/QuestionBox.vue'
+
+export default {
+    name: 'app',
+    components:{
+        Header,
+        QustionBox
+    }
+    
+}
+</script>
+
